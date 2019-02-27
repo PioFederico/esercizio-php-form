89eab5076dd115c491c9cb71a9ae0ddf923f3513
PioFederico <lovecchio.pio.federico@gmail.com>
2019-02-27 10:10:55 +0100

    Inserimento del form base

--- index.php
+++ index.php
@@ -11,8 +11,23 @@
     <title>Hello</title>
   </head>
   <body>
+    <div class="container">
+      <h1>Inserisci i tuoi dati qui</h1>
+      <form>
+        <div class="form-group">
+          <label for="name">Nome</label>
+          <input type="text" class="form-control" id="name" aria-describedby="nameHelp" placeholder="Il tuo nome">
+          <small id="nameHelp" class="form-text text-muted">Scrivi il tuo nome</small>
+        </div>
+        <div class="form-group">
+          <label for="surname">Cognome</label>
+          <input type="text" class="form-control" id="surname" placeholder="Il tuo cognome">
+          <small class="form-text text-muted">Scrivi il tuo cognome</small>
+        </div>
+        <button type="submit" class="btn btn-primary">Iscriviti</button>
+      </form>
+    </div>
 
-    
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="js/jquery-3.3.1.slim.min.js"> </script>
